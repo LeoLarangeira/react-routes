b98b44fb71d287de13489c9914e001ea9f3e775a
LeoLarangeira <leonardo.alves.larangeira@gmail.com>
2024-01-11 22:35:55 -0300

    Start styling the home page for the project

---
+++ src/components/buttons/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const ButtonContainer = styled.button`
+    background-color: #565656;
+    border-radius: 22px;
+    position: relative;
+
+    color: #FFFF;
+    padding: 2px 12px;
+    min-width: 120px;
+    width: 100%;
+
+    
+
+`
