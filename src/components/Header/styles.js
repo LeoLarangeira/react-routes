373d7d4f494a906b9580852bb0e01960024f6315
LeoLarangeira <leonardo.alves.larangeira@gmail.com>
2024-01-17 23:22:41 -0300

    Add content home page

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,13 +3,13 @@ import styled, {css} from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
-    max-width: 90%;
+    max-width: 80%;
     height: 47px;
 
-    display:flex;
+    display: flex;
     justify-content: space-between;
     align-items: center;
-    margin: 0px 12px 0 30px;
+    margin: 0 auto;
 
 
 
@@ -46,11 +46,9 @@ export const Wrapper = styled.div`
     background-color: #151515;
     width: 100%;
     height: 47px;
-    display: flex;
+    display:flex
     justify-content: center;
-    align-items: center;
-
-
+    align-items: center
 `
 
 export const BuscarInputContainer  = styled.div`
@@ -67,11 +65,11 @@ export const BuscarInputContainer  = styled.div`
 export const Menu = styled.a`
     font-family: 'Open Sans';
     font-style: normal;
-    font-size: 12px;
+    font-weight: 700;
+    font-size: 18px;
     line-height: 25px;
     color: #FFFFFF;
-    margin-right: 5px;
-    text-decoration: none;
+    margin-right: 12px;
 
 
 `
@@ -84,23 +82,18 @@ export const MenuRight = styled.a`
     color: #FFFFFF;
     margin-right: 12px;
     text-decoration: none;
-
-
 `
 
 export const UserPicture = styled.img`
-
     width: 32px;
     height: 32px;
     border-radius: 22px;
-    border: 2px solid #FFFFF;
+    border: 3px solid #FFFFFF;
 `
 
 export const Input = styled.input`
-
-    background:transparent;
+    background: transparent;
     flex: 1;
     border: 0;
-    color: white;
-
+    color: #FFFFFF;
 `
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,32 @@
-import { Link } from "react-router-dom"
-import { Button } from "../../components/buttons"
+import { Container, TextContent, Title, TitleHighlight } from "./styles"
+
+import bannerImage from "../../assets/bannerImage.png"
+
 import { HeaderComponent } from "../../components/Header"
+import { Button } from "../../components/buttons"
 const Home = () => {
     return(
         <>
         <HeaderComponent />
-        <Link to='/login'>
-            <Button title= "Teste">fazer Login</Button>
-            <Button variant="secondary" title="Teste 2">Fazer Login variant</Button>
-        </Link>
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighlight>
+                        Implemente
+                        <br />
+                    </TitleHighlight>
+                    o seu futuro é agora!
+                </Title>
+                <TextContent>
+                    Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo 
+                        desafio profissinal, evoluindo em comunidade com os melhores experts
+                </TextContent>
+                <Button title="Começar agora" variant="secondary" onClick={() => null}></Button>
+            </div>
+            <div>
+                <img src={bannerImage} alt="Imagem principal" />
+            </div>
+        </Container>
         </>
     )
 }
